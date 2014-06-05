3961e45f2c3fbd9b258663c19cb6364afa1a6f0f
Milton Dehara <milton9207@outlook.com>
2014-06-05 15:22:28 -0500

    inicializando

---
+++ sfotipy/artists/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Artist
+
+class ArtistAdmin(admin.ModelAdmin):
+	search_fields = ('first_name', 'last_name')
+
+admin.site.register(Artist, ArtistAdmin)
